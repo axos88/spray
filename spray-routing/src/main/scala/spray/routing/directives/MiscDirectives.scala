de70f1ca6dc0c5a4d80d1ca4f2cc673161e17a0b
Akos Vandra <axos88@gmail.com>
2015-10-20 16:35:27 +0200

    validate directive's second parameter should by call-by-name
    The error message should only be evaluated IF an actual error occurs.
    I ran into this by having my string interpolation be based on the fact that an actual error occured:
    
    A similar, dumbed down case would be:
    
    ```
    optionalHeaderValueByName("X-Foo") { foo =>
      validate(foo.map{ value => value == "bar" }, s"Invalid header value: ${header.get}") {
        ...
      }
    }
     ```
    
    The validate would NEVER fail if the header is None, however in that case it will throw a NoSuchElementException because of the interpolation

--- spray-routing/src/main/scala/spray/routing/directives/MiscDirectives.scala
+++ spray-routing/src/main/scala/spray/routing/directives/MiscDirectives.scala
@@ -32,7 +32,7 @@ trait MiscDirectives {
    * Returns a Directive which checks the given condition before passing on the [[spray.routing.RequestContext]] to
    * its inner Route. If the condition fails the route is rejected with a [[spray.routing.ValidationRejection]].
    */
-  def validate(check: ⇒ Boolean, errorMsg: String): Directive0 =
+  def validate(check: ⇒ Boolean, errorMsg: => String): Directive0 =
     new Directive0 {
       def happly(f: HNil ⇒ Route) = if (check) f(HNil) else reject(ValidationRejection(errorMsg))
     }
